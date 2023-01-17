3140a429474a9712246d5be6486cd309435f627b
USUARIO <alvaroacitores@gmail.com>
2023-01-17 18:00:31 +0100

    ugvyc

--- src/matfisplayer1/RobotPlayer.java
+++ src/matfisplayer1/RobotPlayer.java
@@ -13,7 +13,7 @@ import java.util.Set;
  * is created!
  */
 public strictfp class RobotPlayer {
-
+//pucelano el que no bote
     /**
      * We will use this variable to count the number of turns this robot has been alive.
      * You can use static variables like this to save any information you want. Keep in mind that even though
